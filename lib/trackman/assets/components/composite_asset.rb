b6ac872a95610734191469dd3e3c8b35ea187ea7
Jeremy Fabre <jeremy.fabre@hotmail.com>
2012-08-24 21:59:38 -0400

    Change dependencies Removes dup from composite asset old_path as it is not used.

--- lib/trackman/assets/components/composite_asset.rb
+++ lib/trackman/assets/components/composite_asset.rb
@@ -12,7 +12,7 @@ module Trackman
 
         #internals = children_paths.select{|p| p.internal_path? }.map{|p| translate(p, path) }.select{|p| !p.nil? }
         def assets
-          internals = children_paths.select{|p| p.internal_path? }.map{|p| {old: p.dup, new_path: translate(p, path)} }
+          internals = children_paths.select{|p| p.internal_path? }.map{|p| {old: p, new_path: translate(p, path)} }
           internals.select{|p| !p[:new_path].nil? }.map{|p| asset_from(p[:old], p[:new_path])}.inject([]) do |sum, a|
             sum << a
             sum.concat(a.assets.select{|child| !sum.include?(child) })

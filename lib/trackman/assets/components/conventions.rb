c0c8191b35e35e0b71fa61c2c8da5c6c65471e3f
Jeremy Fabre <jeremy.fabre@hotmail.com>
2012-08-26 20:15:28 -0400

    New html assets now have a virtual path

--- lib/trackman/assets/components/conventions.rb
+++ lib/trackman/assets/components/conventions.rb
@@ -11,10 +11,10 @@ module Trackman
           Pathname.new 'public/503-error.html'
         end  
         def maintenance_page
-          Asset.create(:path => maintenance_path)
+          Asset.create(:path => maintenance_path, :virtual_path => maintenance_path)
         end
         def error_page
-          Asset.create(:path => error_path)
+          Asset.create(:path => error_path, :virtual_path => error_path)
         end 
       end
     end
--- lib/trackman/assets/components/diffable.rb
+++ lib/trackman/assets/components/diffable.rb
@@ -3,10 +3,10 @@ module Trackman
     module Components
       module Diffable
         def diff local, remote 
-          to_create = local.select{|a| remote.all? { |s| a.path != s.path } }.map{|a| a.to_remote }.to_a
+          to_create = local.select{|a| remote.all? { |s| a.path != s.path } }.map{|a| a.to_remote }
           { 
             :create => to_create, 
-            :update => remote.select{|a| local.any?{ |s| a.path == s.path && a.file_hash != s.file_hash }}.to_a,  
+            :update => remote.select{|a| local.any?{ |s| a.path == s.path && a.file_hash != s.file_hash }},  
             :delete => define_deleted(local, remote) do |a| 
               to_create.any?{ |c| c.path.basename == a.path.basename }
             end

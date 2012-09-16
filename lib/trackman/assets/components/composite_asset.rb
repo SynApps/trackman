34a040c144f209768cefcc800d565485b8d1fa33
Jeremy Fabre <jeremy.fabre@hotmail.com>
2012-09-16 18:56:24 -0400

    Fix ruby 1.8.7 interpreter

--- lib/trackman/assets/components/composite_asset.rb
+++ lib/trackman/assets/components/composite_asset.rb
@@ -13,7 +13,7 @@ module Trackman
         end
 
         def assets
-          internals = children_paths.select{|p| p.internal_path? }.map{|p| {old: p, new_path: translate(p, path)} }
+          internals = children_paths.select{|p| p.internal_path? }.map{|p| {:old => p, :new_path => translate(p, path)} }
           internals = internals.select{|p| !p[:new_path].nil? }.map{|p| asset_from(p[:old], p[:new_path])}
           internals.inject([]) do |sum, a|
             (sum << a) + a.assets.select{|child| !sum.include?(child) }
